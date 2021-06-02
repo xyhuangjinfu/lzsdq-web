f985b9ef72ac02b7b5dde5d406414a301ac477eb
xyhuangjinfu <630569027@qq.com>
2021-06-02 17:49:17 +0800

    页脚

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,21 +1,21 @@
 <template>
   <div id="footer">
-    <p id="copyright">Copyright © 2021. All Rights Reserved. 版权声明
-本站信息均是网上搜集，如有侵犯您的版权，请及时联系我们，我们将尽快处理。 联系方式 QQ 630569027</p>
+    <div>
+      声明：本站信息如有侵犯您的版权，请及时联系我们，我们将尽快处理。&nbsp;|&nbsp;&nbsp;联系方式：QQ
+      630569027
+    </div>
+    <div>Copyright © 2021. All Rights Reserved.</div>
   </div>
 </template>
 
 <style>
 #footer {
   width: 100%;
-  height: 5em;
   background-color: rgb(70, 70, 70);
-}
-#copyright {
-  width: 20%;
+  text-align: center;
+  padding-top: 2em;
+  padding-bottom: 2em;
   color: rgb(170, 170, 170);
-  margin: 0 auto;
-  position: relative;
-  top: 35%;
+  font-size: 0.8em;
 }
 </style>

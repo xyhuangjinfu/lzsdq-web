9830f34e7bbe0cd1c03e48eeb29cf134fcf99f98
xyhuangjinfu <630569027@qq.com>
2021-06-05 16:23:37 +0800

    热门推荐

--- components/Hot.vue
+++ components/Hot.vue
@@ -1,13 +1,33 @@
 <template>
-  
+  <div id="hot_container">
+    <div>热门推荐 {{ mytitle }}</div>
+    <div id="hot_article_list">
+      <div
+        class="hot_article_item"
+        v-for="article in articles"
+        v-bind:key="article.id"
+      >
+        <router-link
+          class="article_title"
+          v-bind:to="'/a/' + article.id"
+          target="_blank"
+          >{{ article.title }}</router-link
+        >
+      </div>
+    </div>
+  </div>
 </template>
 
 <style>
-
+#hot_container{
+  width: 20%;
+  background-color: rgb(227, 242, 253);
+}
 </style>
 
 <script>
 export default {
+  props: ["mytitle"],
   methods: {
     format_date(d) {
       var newd = new Date(d);
@@ -37,7 +57,7 @@ export default {
   },
   async fetch() {
     this.articles = await fetch(
-      "http://www.lzsdq.cn:9999/api/articles/?page_size=10000000&page_num=1"
+      "http://www.lzsdq.cn:9999/api/articles/hot?limit=5"
     ).then((res) => res.json());
   },
 };

d62e3abc0e80f40377ec2369d3e79ed261e679c6
xyhuangjinfu <630569027@qq.com>
2021-06-06 15:53:46 +0800

    首页重定向

--- pages/index.vue
+++ pages/index.vue
@@ -1,49 +1,14 @@
 <template>
-  <div id="page_container">
-    <Header />
-    <ArticleList id="article_list_container" :pageData="pageData"/>
-    <Footer />
-  </div>
 </template>
 
 <style>
-#page_container {
-  min-height: 100vh;
-  background-color: rgb(247, 247, 247);
-}
-#article_list_container {
-  margin: 0 auto;
-}
 </style>
 
 
 <script>
-import axios from "axios";
-
 export default {
-  data() {
-    return {
-      pageData: {
-        page: 0,
-        totalPage: 0,
-        articles: [],
-        preUrl:"#",
-        nextUrl:"#",
-      },
-    };
-  },
-  async fetch() {
-    this.pageData = await axios
-      .get("http://www.lzsdq.cn:9999/api/articles/?page_size=13&page_num=1")
-      .then((resp) => {
-        return {
-          page: resp.data.page,
-          totalPage: resp.data.totalPage,
-          articles: resp.data.data,
-          preUrl:"#",
-          nextUrl:"/p/2",
-        };
-      });
+  asyncData({ redirect }) {
+    redirect("/p/1");
   },
 };
 </script>

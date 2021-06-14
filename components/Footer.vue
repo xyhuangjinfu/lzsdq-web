34677d8ec5125f18129da5eccf98c1a424ceba06
xyhuangjinfu <630569027@qq.com>
2021-06-14 22:00:12 +0800

    footer

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,21 +1,24 @@
 <template>
-  <div id="footer">
-    <div>
-      声明：本站内容如有侵犯您的版权，请及时联系我们，我们将尽快处理。&nbsp;|&nbsp;&nbsp;联系方式：QQ
-      630569027
+  <div class="footer">
+    <div class="lzsdq-container">
+      <div>
+        声明：本站内容如有侵犯您的版权，请及时联系我们，我们将尽快处理。&nbsp;|&nbsp;&nbsp;联系方式：QQ
+        630569027
+      </div>
+      <div>Copyright 2021 lzsdq.cn 版权所有</div>
     </div>
-    <div>Copyright 2021 lzsdq.cn 版权所有</div>
   </div>
 </template>
 
 <style>
-#footer {
-  width: 100%;
-  background-color: rgb(70, 70, 70);
-  text-align: center;
-  padding-top: 2em;
-  padding-bottom: 2em;
-  color: rgb(170, 170, 170);
-  font-size: 0.8em;
-}
 </style>
+
+<script>
+export default {
+  head() {
+    return {
+      link: [{ rel: "stylesheet", href: require("~/assets/css/footer.css") }],
+    };
+  },
+};
+</script>

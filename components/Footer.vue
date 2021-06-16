246ccbc19041c3f806d45e4ecf4aeb7e70e90170
xyhuangjinfu <630569027@qq.com>
2021-06-16 09:22:40 +0800

    footer样式

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,6 +1,6 @@
 <template>
-  <div class="footer">
-    <div class="lzsdq-container">
+  <div class="footer-container">
+    <div class="footer-content">
       <div>
         声明：本站内容如有侵犯您的版权，请及时联系我们，我们将尽快处理。&nbsp;|&nbsp;&nbsp;联系方式：QQ
         630569027
@@ -17,7 +17,7 @@
 export default {
   head() {
     return {
-      link: [{ rel: "stylesheet", href: require("~/assets/css/footer.css") }],
+      link: [{ rel: "stylesheet", href: require("~/assets/css/components/footer.css") }],
     };
   },
 };

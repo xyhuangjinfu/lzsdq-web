30856bc5872b8a8e1ef126dd0b74c4126c877bb8
xyhuangjinfu <630569027@qq.com>
2021-07-01 17:23:57 +0800

    添加备案号

--- components/Footer.vue
+++ components/Footer.vue
@@ -5,7 +5,14 @@
         声明：本站内容如有侵犯您的版权，请及时联系我们，我们将尽快处理。&nbsp;|&nbsp;&nbsp;联系方式：QQ
         630569027
       </div>
-      <div>Copyright 2021 lzsdq.cn 版权所有</div>
+      <div>
+        Copyright 2021 lzsdq.cn 版权所有&nbsp;&nbsp;|&nbsp;&nbsp;<a
+          class="beian"
+          target="_blank"
+          href="http://beian.miit.gov.cn/"
+          >苏ICP备2021028473号</a
+        >
+      </div>
     </div>
   </div>
 </template>
